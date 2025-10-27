0311893ce1d79ed1d8fb89984b3227d8566be8d0
vismayaunni <vismayaunnikrishnan04@gmail.com>
2025-10-27 08:38:58 +0530

    commit

--- day11clswrk.sql
+++ day11clswrk.sql
@@ -1,13 +1,14 @@
+
 CREATE TABLE authors (
-  author_id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100)
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE books (
-  book_id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(200),
-  author_id INT,
-  FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
 CREATE INDEX idx_author_id ON books(author_id);
